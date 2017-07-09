1d507374d11e50729ca59ed5e6c5e70c024da08c
Vince Gonzalez <vincegonzalez@google.com>
2017-07-08 23:27:40 -0400

    total rewrite

--- injector.py
+++ injector.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python2
+from __future__ import print_function
 
 import argparse
+import csv
+from dateutil import parser as dateparser
+import decimal
 import datetime
 import json
+import random
 import sched
-import time
 import sys
-import decimal
-import dataconverters.commas as commas
-
-from dateutil import parser
+import time
 
 from google.cloud import pubsub
 
+from tabulator import Stream
+from tableschema import infer
+from tableschema import Schema
+from tableschema import exceptions
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -27,112 +32,216 @@ class JSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
+def sample_source(iterable, samplesize):
+    results = []
+    iterator = iter(iterable)
+    # Fill in the first samplesize elements:
+    try:
+        for _ in xrange(samplesize):
+            results.append(iterator.next())
+    except StopIteration:
+        pass
+#        raise ValueError("Sample larger than population.")
+
+    for i, v in enumerate(iterator):
+        r = random.randint(0, i)
+
+        if r < samplesize:
+            results[r] = v  # at a decreasing rate, replace random items
+
+        #if i > samplesize * 1000:
+        #    break
+
+    return results
+
+
+def pubsub_emit_fn(data, arguments):
+    kwargs = arguments
+    try:
+        topic = kwargs["topic"]
+    except KeyError as e:
+        raise ValueError("No topic available in keyword arguments.")
+
+    message = json.dumps(data, cls=JSONEncoder)
+    return topic.publish(message)
+
+
+def default_emit_fn(data, arguments):
+    print(json.dumps(data, cls=JSONEncoder))
+
+
+class emitter:
+    def __init__(self, **kwargs):
+        self.scheduler = sched.scheduler(time.time, time.sleep)
+        self.last_timestamp = None
+        self.topic = kwargs.get("topic", None)
+        self.emit_fn = kwargs.get("fn", default_emit_fn)
+        self.realtime = kwargs.get("realtime", False)
+        self.replay = kwargs.get("replay", False)
+        self.timestamp_field = kwargs.get("timestamp_field")
+
+        self.kwargs = kwargs
+        self.start_time = datetime.datetime.now()
+
+    def emit(self, data):
+        try:
+            # In case date and time are split across columns
+            timestamp_fields = self.timestamp_field.split(',', 2)
+
+            if len(timestamp_fields) == 1:
+                timestamp_string = data[self.timestamp_field]
+            else:
+                timestamp_string = " ".join([data[f] for f in timestamp_fields])
+        except KeyError as e:
+            raise ValueError("Timestamp column '{}' was not found.".format(self.timestamp_field))
+
+        try:
+            timestamp = dateparser.parse(timestamp_string)
+        except ValueError as e:
+            print("Timestamp string '{}' could not be parsed.".format(timestamp_string), file=sys.stderr)
+            return
+
+        now = datetime.datetime.now()
+        new_timestamp = timestamp.replace(year=now.year, month=now.month, day=now.day)
+
+        if self.replay:
+            if new_timestamp < datetime.datetime.now():
+                # If we are replaying events, skip ones that occurred before the current time.
+                return
+
+            t = time.mktime((new_timestamp.year, new_timestamp.month, new_timestamp.day,
+                             new_timestamp.hour, new_timestamp.minute, new_timestamp.second,
+                                 -1, -1, -1)) + new_timestamp.microsecond / 1e6
+            s = self.scheduler.enterabs
+        else:
+            if self.realtime and self.last_timestamp:
+                t = (timestamp - self.last_timestamp).total_seconds()
+            else:
+                t = 0
+            s = self.scheduler.enter
+
+        s(t, 0, self.emit_fn, [data, self.kwargs])
+        self.last_timestamp = timestamp
+
+
+    def run(self):
+        self.scheduler.run()
+
+
+def generate_headers(filename, passed_headers=[], generate_dummy_headers=False):
+    with open(filename) as f:
+        sample = f.read(10240)
+        has_headers = csv.Sniffer().has_header(sample)
+
+    with Stream(filename) as f:
+        if passed_headers:
+            headers = passed_headers.split(",")
+        elif has_headers:
+            headers = [h.strip() for h in f.iter().next()]
+        elif generate_dummy_headers:
+            headers = ["column_{}".format(i) for i,c in enumerate(f.iter().next())]
+        else:
+            print("No headers found. You should pass some in, " +
+                  "or let generate them using --generate-dummy-headers.", file=sys.stderr)
+            raise ValueError("No headers found.")
+    return headers
+
+
+def infer_file_schema(filename, headers=[], infer_row_limit=100):
+    with Stream(filename) as f:
+        sample = sample_source(f, infer_row_limit)
+        return infer(headers, sample, row_limit=infer_row_limit)
+
+
+def injector(filename, inferred_schema=None, headers=None, timestamp_column=None, topic=None, realtime=False, replay=False):
+    with Stream(filename) as f:
+        if inferred_schema:
+            try:
+                schema_obj = Schema(inferred_schema)
+            except exceptions.SchemaValidationError as e:
+                print("Something went wrong inferring the schema. "
+                      "Try not inferring the schema with --no-infer-schema.", file=sys.stderr)
+                print("I'll exit now, since there's nothing more I can do.", file=sys.stderr)
+                sys.exit(1)
+        else:
+            schema_obj = None
+
+        emitter_args = {
+            "realtime": realtime,
+            "replay": replay,
+        }
+
+        if topic:
+            emitter_args.update({"topic": topic, "fn": pubsub_emit_fn})
+        else:
+            emitter_args.update({"fn": default_emit_fn})
+        e = emitter(timestamp_field=timestamp_column, **emitter_args)
+
+        for i, row in enumerate(iter(f.iter())):
+            if schema_obj:
+                row = schema_obj.cast_row(row)
+
+            d = dict(zip(headers, row))
+            e.emit(data=d)
+            if i % 1000 == 0:
+                e.run()
+        e.run()
+
+
 def main():
-    arg_parser = argparse.ArgumentParser(
-        description='Inject CSV or JSON files to Pub/Sub.')
-    arg_parser.add_argument('--filename', type=str, nargs=1, required=True,
-                            help='Filename to inject.')
-    arg_parser.add_argument('--timestamp-field-name', type=str,
-                            default='Timestamp', required=True,
-                            help='Attempt to parse timestamps in this column.')
-    arg_parser.add_argument('--normalize-to-now', action='store_true',
-                            default=False, required=False,
-                            help='Use this option to replay data as though' +
-                                 'it occurred today, in real time.')
-    arg_parser.add_argument('--pubsub-topic', type=str, required=False,
-                            help='Specify a Google Pub/Sub topic to write ' +
-                                 'events to.')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--headers", type=str, required=False,
+                        help="List of headers, if they're not in the file or you want to override them.")
+    parser.add_argument("--infer-row-limit", type=int, required=False, default=100,
+                        help="Set the number of rows to sample for inferring schema.")
+    parser.add_argument("--generate-dummy-headers", action='store_true',
+                        help="Generate column names. Use this if your CSV has no headers.")
+    parser.add_argument("--timestamp-column", type=str, required=True,
+                        help="Set this to the column name that contains the timestamp for the row.")
+    parser.add_argument("--topic", type=str, required=False,
+                        help="Emit rows to Google Cloud Pubsub.")
+    parser.add_argument("--realtime", action='store_true', required=False,
+                        help="Emit rows using actual deltas between events.")
+    parser.add_argument("--replay", action='store_true', required=False,
+                        help="If set, replace the date portion of the timestamp with today, " \
+                             "and emit events occurring after the current time.")
+    parser.add_argument("--no-infer-schema", action='store_true', required=False, default=False,
+                        help="If schema inference is failing, disable it with this option.")
+    parser.add_argument("--file", type=str, required=True,
+                        help="Filename of file to inject.")
 
-    args = arg_parser.parse_args(sys.argv[1:])
+    args = parser.parse_args(sys.argv[1:])
 
-    filename = args.filename[0]
-
-    timestampFieldName = args.timestamp_field_name
-    normalizeToNow = args.normalize_to_now
+    filename = args.file
+    timestamp_column = args.timestamp_column
 
     topic = None
-    if args.pubsub_topic:
-        client = pubsub.Client()
-        topic = client.topic(args.pubsub_topic)
-
-    if filename.endswith("json"):
-        lines = iter(open(filename, "r").readlines())
-    else:
-        lines = (csvToJson(filename))
-
-    scheduler = sched.scheduler(time.time, time.sleep)
-    start_time = None
-    line_count = 0
-    for line in lines:
+    if args.topic:
         try:
-            d = json.loads(line)
-        except ValueError as e:
-            print(e)
-            continue
-        try:
-            timestampFields = timestampFieldName.split(',', 2)
-            t = tuple([(d.get(f)) for f in timestampFields])
-            current_timestamp = parser.parse(t[0])
-            if normalizeToNow:
-                if len(t) == 2:
-                    d[timestampFields[0]] = current_timestamp.date().strftime('%Y-%m-%d')
-                else:
-                    d[timestampFields[0]] = current_timestamp.isoformat()
-            if len(t) == 2:
-                tt = parser.parse(t[1]).time()
-                current_timestamp = current_timestamp.replace(
-                    hour=tt.hour, minute=tt.minute, second=tt.second)
-                if normalizeToNow:
-                    d[timestampFields[1]] = current_timestamp.time().isoformat()
-        except KeyError as e:
-            print("The specified timestamp key '{}' " +
-                  "does not exist in this line: {}"
-                  .format(timestampFieldName, line))
-            raise
+            client = pubsub.Client()
+            topic = client.topic(args.topic)
+        except Exception as e:
+            print("Exception caught when connecting to PubSub: {}".format(e.message))
+            sys.exit(1)
 
-        if start_time is None:
-            start_time = datetime.datetime.now(current_timestamp.tzinfo).time()
+    headers = generate_headers(filename,
+                               passed_headers=args.headers,
+                               generate_dummy_headers=args.generate_dummy_headers)
 
-        if not normalizeToNow:
-            current_timestamp = datetime.datetime.now()
-        else:
-            now = datetime.datetime.now(current_timestamp.tzinfo)
-            current_timestamp = current_timestamp.replace(
-                year=now.year, month=now.month, day=now.day)
-
-        if normalizeToNow and current_timestamp.time() < start_time:
-            continue
-
-        scheduleMessage(int(current_timestamp.strftime("%s")),
-                        [json.dumps(d, sort_keys=True, separators=(',', ':'))],
-                        topic=topic, scheduler=scheduler)
-
-        line_count += 1
-        if line_count % 1000 == 0:
-            scheduler.run()
-        scheduler.run()
-
-
-def scheduleMessage(timestamp, message, topic=None, scheduler=None):
-    if topic:
-        fn = topic.publish
+    if args.no_infer_schema:
+        inferred_schema = None
     else:
-        fn = emitFunction
+        inferred_schema = infer_file_schema(filename,
+                                            headers=headers,
+                                            infer_row_limit=args.infer_row_limit)
 
-    print("Scheduling for {}\n{}".format(
-        datetime.datetime.fromtimestamp(timestamp), message))
-    scheduler.enterabs(time=timestamp, priority=1, action=fn, argument=message)
-
-
-def csvToJson(inputFilename):
-    with open(inputFilename, "rb") as f:
-        records, metadata = commas.parse(f, guess_types=True)
-    return iter([json.dumps(r, cls=JSONEncoder) for r in records])
-
-
-def emitFunction(jsonString):
-    print(jsonString)
+    injector(filename,
+             inferred_schema=inferred_schema,
+             headers=headers,
+             timestamp_column=timestamp_column,
+             topic=topic,
+             replay=args.replay,
+             realtime=args.realtime)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": main()
